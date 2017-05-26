4b21ec39a994fc35504e7c2dc4abd57d1dda1cb7
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2017-05-26 14:42:24 +0600

    add debugging plugin

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -53,3 +53,8 @@ fi
 
 cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+noroot wp plugin install developer --activate
+noroot wp plugin install debug-bar --activate
+noroot wp plugin install query-monitor --activate
+noroot wp plugin install user-switching --activate
