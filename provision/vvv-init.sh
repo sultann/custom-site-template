6f9f37bd7967de33677c0bcd057e2869c36d8e2a
MD Sultan Nasir Uddin <manikdrmc@gmail.com>
2017-05-28 22:28:00 +0600

    changed root directory

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -21,12 +21,12 @@ touch ${VVV_PATH_TO_SITE}/log/error.log
 touch ${VVV_PATH_TO_SITE}/log/access.log
 
 # Install and configure the latest stable version of WordPress
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-load.php" ]]; then
+if [[ ! -f "${VVV_PATH_TO_SITE}/htdocs/wp-load.php" ]]; then
     echo "Downloading WordPress..."
 	noroot wp core download --version="${WP_VERSION}"
 fi
 
-if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/wp-config.php" ]]; then
+if [[ ! -f "${VVV_PATH_TO_SITE}/htdocs/wp-config.php" ]]; then
   echo "Configuring WordPress Stable..."
   noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
 define( 'WP_DEBUG', true );
@@ -47,7 +47,7 @@ if ! $(noroot wp core is-installed); then
   noroot wp core ${INSTALL_COMMAND} --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
 else
   echo "Updating WordPress Stable..."
-  cd ${VVV_PATH_TO_SITE}/public_html
+  cd ${VVV_PATH_TO_SITE}/htdocs
   noroot wp core update --version="${WP_VERSION}"
 fi
 
